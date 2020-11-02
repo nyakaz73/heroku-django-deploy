74bb5c5831606513c1a665297bc8f704d91bab56
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-11-02 10:51:31 +0200

    Added deployWith Git command

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const core = require('@actions/core');
 const { execSync, exec } = require('child_process')
-//Variables
-/*let heroku = {
-    'app_name': 'django-heroku-app001',
-    'api_key': 'c8cc8d0dc5-b53b-4d03-812e-1c09582e8fa4',
-    'email_address': 'tafadxzwalnyamukapa@gmail.com',
-    'want_to_login': true
-}*/
+
+//Variables.
 let heroku = {
     'app_name': core.getInput('heroku_app_name'),
     'api_key': core.getInput('heroku_api_key'),
     'email_address': core.getInput('heroku_email_address'),
-    'want_to_login': core.getInput('want_to_login')
+    'want_to_login': core.getInput('want_to_login'),
+    'use_git': core.getInput('use_git'),
+    'use_docker': core.getInput('use_docker'),
 }
 
+//Create Netrc cat file used during login with cli.
 const createNetrcFileForLogin = ({ email_address, api_key }) => {
     return execSync(`cat >~/.netrc <<EOF
 machine api.heroku.com
@@ -24,6 +22,7 @@ machine git.heroku.com
   password ${api_key}
         `);
 }
+//Login method to check if user is logged in.
 const login = () => {
     try {
         createNetrcFileForLogin(heroku);
@@ -35,25 +34,71 @@ const login = () => {
     }
 }
 
-const addRemote = () => {
+//Adding remote repo to heroku.
+const addRemote = ({ app_name }) => {
     try {
         const gitInit = execSync('git init').toString();
         console.log(gitInit);
-        console.log('Initialized git');
-
-
-
+        execSync(`heroku git:remote -a ${app_name}`);
     } catch (error) {
+        core.setFailed(error.message);
+        console.log(error.message);
+    }
+}
 
+
+const deployWithDocker = () => {
+
+}
+const deployWithGit = () => {
+    try {
+        execSync("git add .");
+        execSync(`git commit -m "Initial commit" `);
+        execSync("git push heroku master");
+        execSync("heroku run python manage.py migrate");
+    } catch (error) {
+        console.log(error.message);
+        console.log("Attempting to disable `collecstatic` cmd");
+        try {
+            execSync("git config:set DISABLE_COLLECTSTATIC=1");
+            execSync("git push heroku master");
+            execSync("heroku run python manage.py migrate");
+        } catch (error) {
+            core.setFailed(error.message);
+            console.log(error.message);
+        }
+    }
+}
+const pushAndRelease = ({ use_docker, use_git }) => {
+    try {
+        if (use_docker) {
+            deployWithDocker();
+        } else if (use_git) {
+            deployWithGit();
+        }
+        else if (use_docker && use_git) {
+            //Error only one deployment method at a time is allowed
+            core.setFailed('Error : One deployment method at a time is allowed');
+            console.log('Error : One deployment method at a time is allowed');
+        } else {
+            //Nothing is configured attempting to use default git
+            console.log('No deplyment method is configured. Attempting to use default git...');
+            deployWithGit();
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+        console.log(error.message);
     }
 }
 
 //Run Login
 if (heroku.want_to_login) {
     login();
+    //return , use return if user jus want to login
 }
 
-addRemote();
+addRemote(docker);
+pushAndRelease(docker);
 
 core.setOutput(
     "status",
