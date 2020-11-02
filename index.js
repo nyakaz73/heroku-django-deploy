8973d5980932e59ce8e562318e38c6b4dbd6f488
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-11-02 19:38:59 +0200

    Updated gitstatus

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const addRemote = ({ app_name }) => {
         const gitInit = execSync('git init').toString();
         console.log(gitInit);
         const remote = execSync(`heroku git:remote -a ${app_name}`).toString();
-        console.log(remote.message);
+        console.log(remote);
     } catch (error) {
         core.setFailed(error.message);
         console.log(error.message);
@@ -58,6 +58,7 @@ deployWithGit = () => {
         execSync(`git config user.email "${heroku.email_address}"`);
         const add = execSync("git add -A").toString();
         console.log(add);
+        execSync("git status")
         execSync('git commit -m "Initial commit" ').toString();
         const push = execSync("git push heroku master").toString();
         console.log(push);
