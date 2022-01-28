5ac65bd7db46aae897047f7fb9ab3ec5992d85e7
Lameck <lameck.nyamukapa@trustgro.com>
2022-01-28 16:49:24 +0300

    Reverted to master

--- index.js
+++ index.js
@@ -55,10 +55,13 @@ checkIfRepoIsShallow = () => {
 };
 
 gitForcePush = () => {
-  //execSync("git pull heroku main", execOptions)
+  //execSync("git pull heroku master", execOptions)
   //const pull = execSync("git pull --ff-only", execOptions).toString();
   //console.log(pull);
-  const push = execSync("git push --force heroku main", execOptions).toString();
+  const push = execSync(
+    "git push --force heroku master",
+    execOptions
+  ).toString();
   console.log(push);
 };
 herokuForcePush = ({ app_name }) => {
@@ -69,7 +72,10 @@ herokuForcePush = ({ app_name }) => {
   console.log(push);
 };
 manifestForcePush = () => {
-  const push = execSync("git push --force heroku main", execOptions).toString();
+  const push = execSync(
+    "git push --force heroku master",
+    execOptions
+  ).toString();
   console.log(push);
 };
 disableCollectStatic = () => {
@@ -126,7 +132,7 @@ deployWithBuildManifest = () => {
     if (heroku.force_push === "true") {
       manifestForcePush();
     } else {
-      const push = execSync("git push heroku main", execOptions);
+      const push = execSync("git push heroku master", execOptions);
       console.log(push);
     }
   } catch (error) {
@@ -193,7 +199,7 @@ deployWithGit = () => {
     if (heroku.force_push === "true") {
       gitForcePush();
     } else {
-      const push = execSync("git push heroku main", execOptions).toString();
+      const push = execSync("git push heroku master", execOptions).toString();
       console.log(push);
     }
     const migrate = execSync(
