87c00a7b1f87cbb061b07f342f702c45df2d77f3
Lameck <lameck.nyamukapa@trustgro.com>
2022-01-29 18:23:04 +0300

    Initialized branch to main

--- index.js
+++ index.js
@@ -85,10 +85,12 @@ addRemote = ({ app_name }) => {
     const gitInit = execSync("git init", execOptions).toString();
     console.log(gitInit);
     //Setting default branch to main
+    console.log("initializing branch");
     const initBranch = execSync(
       "git config --global init.defaultBranch main",
       execOptions
     ).toString();
+    console.log("After initalsing branch");
     console.log(initBranch);
     const remote = execSync(
       `heroku git:remote -a ${app_name}`,
